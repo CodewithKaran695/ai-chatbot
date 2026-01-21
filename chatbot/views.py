a91276bdb54bb23e79fc5339306560bb1893ca1c
CodewithKaran695 <codewithkaran695@gmail.com>
2026-01-21 20:50:43 +0530

    initial level of chatbot project

---
+++ chatbot/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from groq import Groq
+import os
+# Create your views here.
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+def chat_page(request):
+    user_message = None
+    ai_reply = None
+
+    if request.method == 'POST':
+        user_message = request.POST.get("message")
+
+        if user_message:
+            response = client.chat.completions.create(
+    model="llama-3.1-8b-instant",
+    messages=[
+        {
+            "role": "system",
+            "content": (
+                "You are a helpful assistant. "
+                "Format responses clearly using short paragraphs, bullet points, "
+                "headings where appropriate, and line breaks for readability."
+            )
+        },
+        {"role": "user", "content": user_message}
+    ]
+)
+            
+
+            ai_reply = response.choices[0].message.content
+
+    return render(request, "chatbot/chat.html", {
+        "user_message": user_message,
+        "ai_reply": ai_reply
+    })
